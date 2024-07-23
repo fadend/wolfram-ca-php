78cf45ab5bd4150d5ee822a463948efb314aee3a
David Faden (revfad.com) <dfaden@gmail.com>
2024-07-23 11:18:48 -0700

    Support starting with just the leftmost, middle, or rightmost cell on.
    Also fix bug in image header. There was a typo, "imager" instead of "image",
    explaining why it was iniating a download instead of getting displayed inline
    in the tab when viewed alone.

--- wolfram_ca.php
+++ wolfram_ca.php
@@ -6,14 +6,19 @@
 // http://www.wolframscience.com/nksonline/page-24
 //
 // This script looks for the following parameters, all of which are optional:
+//   rule -- coding of the update function
 //   cells -- the number of cells
 //   steps -- the number of times to iterate the update function on the cells
+//   start_type -- how to initialize the cells
 //   initial -- the initial values for the cells
 //   seed -- the seed for the random number generator used to
 //         fill in the initial values of the cells if initial isn't provided
-//   rule -- coding of the update function
+
+declare(strict_types=1);
 
 require_once('get_param.php');
+
+const START_TYPES = ['left', 'middle', 'multiple', 'right'];
 
 function printRuleDescriptionHtml(int $rule)
 {
@@ -30,6 +35,40 @@ function printRuleDescriptionHtml(int $rule)
         echo "$neighborhoodString&nbsp;to&nbsp;$value";
     }
     echo '.';
+}
+
+function createInitialCells(string $startType, int $numberOfCells, string $initialString = '', int $seed = 0): array
+{
+    $cells = array_fill(0, $numberOfCells, 0);
+    switch ($startType) {
+        case 'left':
+            $cells[0] = 1;
+            break;
+        case 'right':
+            $cells[$numberOfCells - 1] = 1;
+            break;
+        case 'middle':
+            $cells[intdiv($numberOfCells, 2)] = 1;
+            break;
+        case 'multiple':
+            if ($initialString) {
+                $length = strlen($initialString);
+                for ($i = 0; $i < $numberOfCells; $i++) {
+                    $cells[$i] = intval('1' == $initialString[$i % $length]);
+                }
+            } else {
+                if ($seed) {
+                    mt_srand($seed);
+                }
+                for ($c = 0; $c < $numberOfCells; $c++) {
+                    $cells[$c] = mt_rand(0, 1);
+                }
+            }
+            break;
+        default:
+            throw new InvalidArgumentException("Unknown startType: $startType");
+    }
+    return $cells;
 }
 
 function sendCellularAutomataStepsImage(array $cellsInitialState, int $rule, int $numberOfSteps)
@@ -59,17 +98,7 @@ function sendCellularAutomataStepsImage(array $cellsInitialState, int $rule, int
         }
     }
 
-    header('Content-type: imager/png', true);
-    // TODO: Make it so that when users select
-    // "Open image in new window" or the equivalent from their
-    // browser window, the image will indeed be opened within
-    // the window. The RFC for Content-Disposition suggests
-    // that the following would work, but it doesn't.
-    // Currently, Safari downloads the image to disk rather
-    // than displaying it inline. Firefox does the same.
-    // At least with the Content-Disposition header
-    // we can specify a reasonable name for the downloaded file.
-    // See http://www.faqs.org/rfcs/rfc2183.
+    header('Content-type: image/png', true);
     header("Content-Disposition: inline; filename=\"rule$rule.png\"");
 
     imagePNG($image);
@@ -77,9 +106,13 @@ function sendCellularAutomataStepsImage(array $cellsInitialState, int $rule, int
 }
 
 //TODO: Warn the user when a parameter is invalid.
-$numberOfCells = int_param_with_default_range('cells', 200, 1, 1000);
-$numberOfSteps = int_param_with_default_range('steps', 200, 1, 1000);
+$numberOfCells = int_param_with_default_range('cells', 200, 1, 2000);
+$numberOfSteps = int_param_with_default_range('steps', 200, 1, 2000);
 $rule = int_param_with_default('rule', 110);
+$startType = get_with_default('start_type', 'multiple');
+if (!in_array($startType, START_TYPES, true)) {
+    $startType = 'multiple';
+}
 $initialString = '';
 if (isset($_GET['initial']) && preg_match('/^[01]+$/', $_GET['initial'])) {
     $initialString = $_GET['initial'];
@@ -87,20 +120,8 @@ if (isset($_GET['initial']) && preg_match('/^[01]+$/', $_GET['initial'])) {
 $seed = int_param_with_default('seed', 0);
 
 if (get_with_default('image', 'no') == 'yes') {
-    $cells = array();
-    if ($initialString) {
-        $length = strlen($initialString);
-        for ($i = 0; $i < $numberOfCells; $i++) {
-            $cells[$i] = intval('1' == $initialString[$i % $length]);
-        }
-    } else {
-        if ($seed) {
-            mt_srand($seed);
-        }
-        for ($c = 0; $c < $numberOfCells; $c++) {
-            $cells[$c] = mt_rand(0, 1);
-        }
-    }
+    $cells = createInitialCells($startType, $numberOfCells, $initialString, $seed);
+
     sendCellularAutomataStepsImage($cells, $rule, $numberOfSteps);
 } else {
     header('Content-type: text/html', true);
@@ -111,7 +132,7 @@ if (get_with_default('image', 'no') == 'yes') {
 <div style="width:450px">
 <h1>Rule <?= $rule ?></h1>
 <img
-  src="<?= "wolfram_ca.php?cells=$numberOfCells&steps=$numberOfSteps&rule=$rule&seed=$seed&initial=$initialString&image=yes" ?>"
+  src="<?= "wolfram_ca.php?cells=$numberOfCells&steps=$numberOfSteps&rule=$rule&start_type=$startType&seed=$seed&initial=$initialString&image=yes" ?>"
   width="<?= $numberOfCells ?>"
   height="<?= $numberOfSteps ?>"
   alt="Image of rule <?= $rule ?>"
@@ -120,28 +141,76 @@ if (get_with_default('image', 'no') == 'yes') {
 <?php printRuleDescriptionHtml($rule); ?>
 <h2>Change the parameters</h2>
 <form method="get">
-Rule: <input type="text" value="<?= $rule ?>" name="rule">
+Rule: <input type="text" name="rule" value="<?= $rule ?>">
 <br>
-Number of cells: <input type="text" value="<?= $numberOfCells ?>" name="cells">
+Number of cells: <input type="text" name="cells" value="<?= $numberOfCells ?>">
 <br>
-Number of steps: <input type="text" value="<?= $numberOfSteps ?>" name="steps">
+Number of steps: <input type="text" name="steps" value="<?= $numberOfSteps ?>">
 <br>
-Random seed: <input type="text" value="<?= $seed ?>" name="seed">
+Initial state:
 <br>
-Initial cell values: <input type="text" value="<?= $initialString ?>" name="initial" size="35">
-&nbsp;&nbsp;<input type="submit">
+<label>
+  <input type="radio" name="start_type" id="start-type-multiple" value="multiple"
+    <?= ($startType == 'multiple') ? 'checked' : '' ?> >
+  Start with multiple cells "on" (selected at random or from given initialization string)
+</label>
+<br>
+<label>
+  <input type="radio" name="start_type" value="left"
+    <?= ($startType == 'left') ? 'checked' : '' ?> >
+  Start with only the leftmost cell "on".
+</label>
+<br>
+<label>
+  <input type="radio" name="start_type" value="middle"
+    <?= ($startType == 'middle') ? 'checked' : '' ?> >
+  Start with only the middle cell "on".
+</label>
+<br>
+<label>
+  <input type="radio" name="start_type" value="right"
+    <?= ($startType == 'right') ? 'checked' : '' ?> >
+  Start with only the rightmost cell "on".
+</label>
+<div id="options-for-multiple">
+Random seed: <input type="text" name="seed" value="<?= $seed ?>">
+<br>
+Initial cell values: <input type="text" name="initial" value="<?= $initialString ?>" size="35">
+</div>
+<br>
+<input type="submit">
 </form>
 <hr>
-<p>If no initial value is provided, the cells are set to random values. If an initial value isn't provided for every cell, the provided sequence of values is repeated until every cell is provided for, going from left to right.
+<p>
+  If you select the option to start with (potentially) multiple cells initially "on", then if
+  provided, the "Initial cell values" string will be repeated to fill the entire row of cells.
+  If you leave "Initial cell values" empty, the cells will be pseudo randomly initialized
+  instead.
 </p>
 <p>
-If a non-zero seed is provided, it will be used to seed the pseudo-random number generator. It's useful to specify a seed here if you wish the results to be easily reproducible.
+  Set a non-zero seed if you want the pseudo random initialization to be consistent across reloads.
 </p>
 <p>
-More information on cellular automata and their applications can be found in Stephen Wolfram's
-<a href="http://www.wolframscience.com/nksonline/toc.html">A New Kind of Science</a>.
+  More information on cellular automata and their applications can be found in Stephen Wolfram's
+  <a href="http://www.wolframscience.com/nksonline/toc.html">A New Kind of Science</a>.
+  This page uses the encoding described in
+  <a href="https://www.wolframscience.com/nks/p51--the-search-for-general-features/">Chapter 3</a>
+  of his book.
 </p>
 </div>
+<script>
+  (function() {
+    const optionsForMultipleDiv = document.getElementById('options-for-multiple');
+    const multipleRadio = document.getElementById('start-type-multiple');
+    let hideShowMultipleOptions = () => {
+        optionsForMultipleDiv.style.display = multipleRadio.checked ? '' : 'none';
+    };
+    for (let radio of [...document.getElementsByName('start_type')]) {
+        radio.addEventListener('click', hideShowMultipleOptions);
+    }
+    hideShowMultipleOptions();
+  })();
+</script>
 </body>
 </html>
 <?php } ?>
